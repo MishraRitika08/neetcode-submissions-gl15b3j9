95e198ec8f3786bf87d9faa3a689544a3f3ba7ac
Ritika Mishra <132245230+MishraRitika08@users.noreply.github.com>
2026-04-04 17:09:48 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,34 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int profit = 0;
+//         int buyPrice = prices[0];
+//         for(int i = 1; i<prices.length;i++){
+//             //profit
+//             if(buyPrice<prices[i]){
+//                 int currP = prices[i]-buyPrice;
+//                 profit = Math.max(profit,currP);
+//             }
+//             else{
+//                 buyPrice = prices[i];
+//             }            
+//         }
+//     return profit;
+//     }
+// }
+class Solution {
+    public int maxProfit(int[] prices) {
+        int bp = prices[0];
+        int profit = 0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<bp){
+                bp = prices[i];
+            }
+            else{
+                int curr = prices[i] - bp;
+                profit = Math.max(curr, profit);
+            }
+
+        }
+        return profit;
+    }
+}
